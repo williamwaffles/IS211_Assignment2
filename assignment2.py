c8cf38c0143e6c2a3dbe379d0e044cd370126ee2
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-09-14 22:16:16 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -7,7 +7,7 @@ import datetime
 user_url = urllib.request.Request('http://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv')
 
 #function that downloads the needed data
-def downloadData(url = 'http://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'):
+def downloadData(url):
     with urllib.request.urlopen(url) as response:
         content = response.read().decode('utf-8')
         return content
@@ -16,29 +16,25 @@ def downloadData(url = 'http://s3.amazonaws.com/cuny-is211-spring2015/birthdays1
 def processData(file_content):
     parsed_dict ={}
     header = True
-    for data in file_content.split('\n'):
+    for data in file_content.split('\n')[:-1]:
         if header:
             header = False
             continue
         else:
             parsed_data = data.split(',')
+            #stripped_date = datetime.datetime.strptime((parsed_data[2]), '%d/%m/%Y')
+            #print(stripped_date)
             parsed_dict[parsed_data[0]] = (parsed_data[1], parsed_data[2])
-            print(parsed_dict)
-            continue
+            #print(parsed_dict)
     return parsed_dict
 
     #format = "%d %m %y"
     #list_data = datetime.datetime.strptime(index, format)
         #print(i)
-'''
-    with file_content as f:
-        reader = list(f)
-        print(map(lambda x: dict(zip(reader[0], x)), reader))
- '''
 
 print(downloadData(user_url))
 
-processData(downloadData(user_url))
+print(processData(downloadData(user_url)))
 
 
 def displayPerson(id, personData):
